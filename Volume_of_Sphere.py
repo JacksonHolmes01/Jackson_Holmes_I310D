1fe6718e5338d49c803d2f2f4a91944ea370c2ac
Jackson Holmes <jbirdholmes@icloud.com>
2023-09-14 17:29:17 -0500

    I changed the radius' to 40 and to 50

--- Volume_of_Sphere.py
+++ Volume_of_Sphere.py
@@ -4,10 +4,10 @@ def compute_volume_of_sphere(radius):
     volume = 4/3 * pi * radius * radius
     return volume
 
-radius1 = 30
+radius1 = 40
 volume1 = compute_volume_of_sphere(radius1)
 print(f"The volume of sphere with radius {radius1} is: {volume1}")
 
-radius2 = 40
+radius2 = 50
 volume2 = compute_volume_of_sphere(radius2)
 print(f"The volume of sphere with radius {radius2} is: {volume2}")
